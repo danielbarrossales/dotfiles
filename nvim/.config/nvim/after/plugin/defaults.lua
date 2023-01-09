735b40d6ff05fbe7c889d6cdb7ce91bf99616a22
Daniel Barros Sales <daniel.b.sales@outlook.com>
2023-01-09 00:12:05 -0300

    Enable wayland system clipboard integration

--- nvim/.config/nvim/after/plugin/defaults.lua
+++ nvim/.config/nvim/after/plugin/defaults.lua
@@ -1,29 +1,19 @@
-local api = vim.api
 local g = vim.g
 local opt = vim.opt
-
--- Remap leader and local leader to <Space>
-api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
-g.mapleader = " "
-g.maplocalleader = " "
+local api = vim.api
+local keymap = vim.keymap
 
 opt.termguicolors = true -- Enable colors in terminal
-opt.hlsearch = true --Set highlight on search
-opt.number = true --Make line numbers default
+opt.hlsearch = true -- Set highlight on search
+opt.number = true -- Make line numbers default
 opt.relativenumber = true --Make relative number default
 opt.mouse = "a" --Enable mouse mode
 opt.breakindent = true --Enable break indent
-opt.undofile = true --Save undo history
-opt.ignorecase = true --Case insensitive searching unless /C or capital in search
-opt.smartcase = true -- Smart case
-opt.updatetime = 250 --Decrease update time
-opt.signcolumn = "yes" -- Always show sign column
-opt.clipboard = "unnamedplus" -- Access system clipboard
+opt.undofile = true --Enable undo history
+opt.ignorecase = true --Case insensitive searching unless /C or capital ins earch
+opt.smartcase = true --Case insensitive unless captal letter
+opt.updatetime = 250 --Time waited before CursorHold or writing to swap file
+opt.signcolumn = "yes"
+opt.clipboard = "unnamedplus"
 
--- Highlight on yank
-vim.cmd [[
-  augroup YankHighlight
-    autocmd!
-    autocmd TextYankPost * silent! lua vim.highlight.on_yank()
-  augroup end
-]]
+keymap.set({'n', 'v'}, 'y', "\"+y")
