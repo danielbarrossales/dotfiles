ec7c0234b8fa173b42c2fb2e576ee5ff3cbc5677
Daniel Barros Sales <daniel.b.sales@outlook.com>
2023-11-24 18:44:55 -0300

    fix: modify lsp

--- neovim/.config/nvim/lua/config/plugin/lsp.lua
+++ neovim/.config/nvim/lua/config/plugin/lsp.lua
@@ -56,7 +56,13 @@ function M.setup()
     require('mason-lspconfig').setup({
         -- Replace the language servers listed here 
         -- with the ones you want to install
-        ensure_installed = {'tsserver', 'rust_analyzer', "lua_ls", "csharp_ls", "eslint" },
+        ensure_installed = {
+            'tsserver',
+            'rust_analyzer',
+            "lua_ls",
+            "csharp_ls",
+            "eslint"
+        },
         handlers = {
             lsp.default_setup,
         },
