57fcb257ff9d0305bfbedae87141b313af79c108
Daniel Barros Sales <daniel.b.sales@outlook.com>
2024-01-14 03:26:15 -0300

    nvim: remove all previous config

--- neovim/.config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
-local g = vim.g
-
-g.maplocalleader = " "
-g.mapleader = " "
-vim.api.nvim_set_keymap('', '<Space>', '<Nop>', { noremap = true, silent = true })
-
-require("plugins").setup()
-require("keymaps").setup()
