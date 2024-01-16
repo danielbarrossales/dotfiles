f95aaee46cd8d686c9ca6d3f7d5f77798ac95e54
Daniel Barros Sales <daniel.b.sales@outlook.com>
2024-01-16 08:13:49 -0300

    nvim: lazyvim config

---
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,35 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+--local keymap = vim.keymap
+--local opts = { noremap = true, silent = true }
+--
+---- Increment/decrement
+--keymap.set("n", "+", "<C-a>")
+--keymap.set("n", "-", "<C-x>")
+--
+---- Select all
+--keymap.set("n", "<C-a>", "gg<S-v>G")
+--
+---- Tab
+--keymap.set("n", "te", ":tabedit<Return>", opts)
+--keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+--keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+--keymap.set("n", "td", ":bd<Return>", opts)
+--
+---- Split window
+--keymap.set("n", "ss", ":split<Return>", opts)
+--keymap.set("n", "sv", ":vsplit<Return>", opts)
+--
+----Move window
+--keymap.set("n", "sh", "<C-w>h", opts)
+--keymap.set("n", "sj", "<C-w>j", opts)
+--keymap.set("n", "sk", "<C-w>k", opts)
+--keymap.set("n", "sl", "<C-w>l", opts)
+--
+---- Resize window
+--keymap.set("n", "<C-w>h", "4<C-w><")
+--keymap.set("n", "<C-w>j", "2<C-w>+")
+--keymap.set("n", "<C-w>k", "2<C-w>-")
+--keymap.set("n", "<C-w>l", "4<C-w>>")
---
+++ neovim/.config/nvim/lua/plugins/neorg.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              notes = "~/Documents/neorg/notes",
+            },
+          },
+        },
+      },
+    },
+  },
+}
