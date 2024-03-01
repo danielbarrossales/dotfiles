df7859853ebeaf6a227d1473b6cd23befd15b0b5
Daniel Barros Sales <daniel.b.sales@outlook.com>
2024-03-01 20:47:20 -0300

    config(nvim): add shortcut to dashboard and update plugins

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -37,6 +37,22 @@ return {
                         local footer = vim.list_extend(info, fortune)
                         return footer
                     end,
+                    shortcut = {
+                        { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+                        {
+                            icon = ' ',
+                            icon_hl = '@variable',
+                            desc = 'Files',
+                            group = 'Label',
+                            action = 'Telescope find_files',
+                            key = 'f',
+                        },
+                        {
+                            desc = "Quit",
+                            action = "q",
+                            key = "q",
+                        }
+                    },
                 }
             }
         end,
