fd5f58b73f33c732675e5390129f08e35dbda769
Daniel Barros Sales <daniel.b.sales@outlook.com>
2024-03-06 23:43:03 -0300

    config(nvim): customize navigation shortcuts and neovim appearance

--- neovim/.config/nvim/after/plugin/default.lua
+++ neovim/.config/nvim/after/plugin/default.lua
@@ -1,6 +1,6 @@
 local o = vim.o
-o.tabstop = 4
-o.shiftwidth = 4
+o.tabstop = 2
+o.shiftwidth = 2
 o.expandtab = true
 o.smartindent = true
 o.number = true
@@ -10,3 +10,13 @@ o.termguicolors = true
 local opt = vim.opt
 opt.colorcolumn = "80"
 vim.api.nvim_set_hl(0, "ColorColumn", { ctermbg = 240, bg = "Grey" })
+
+opt.ignorecase = true
+opt.smartcase = true
+opt.termguicolors = true
+opt.signcolumn = "yes"
+opt.background = "dark"
+opt.backspace = "indent,eol,start"
+opt.clipboard:append("unnamedplus")
+
+opt.iskeyword:append("-")
--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -8,9 +8,31 @@ function M.setup()
     keymap.set("n", "<leader>qq", "<cmd>q<cr>", { desc = "Quit Neovim" })
 
     -- Move to direction
-    keymap.set( { "n", "t" }, "<C-h>", "<C-w>h", { desc = { "Go to left window" }})
-    keymap.set( { "n", "t" }, "<C-j>", "<C-w>j", { desc = { "Go to lower window" }})
-    keymap.set( { "n", "t" }, "<C-k>", "<C-w>k", { desc = { "Go to upper window" }})
-    keymap.set( { "n", "t" }, "<C-l>", "<C-w>l", { desc = { "Go to right window" }})
+    keymap.set( { "n", "t" }, "<C-h>", "<C-w>h", { desc = "Go to left window" })
+    keymap.set( { "n", "t" }, "<C-j>", "<C-w>j", { desc = "Go to lower window" })
+    keymap.set( { "n", "t" }, "<C-k>", "<C-w>k", { desc = "Go to upper window" })
+    keymap.set( { "n", "t" }, "<C-l>", "<C-w>l", { desc = "Go to right window" })
+
+    -- Clear selection
+    keymap.set("n", "<leader>nh", "<CMD>nohl<CR>", { desc = "Clear selection"})
+
+    -- When delete char, don't copy to register
+    keymap.set("n", "x", '"_x')
+
+    -- Increment/decrement
+    keymap.set("n", "<LEADER>+", "<C-a>", { desc = "Increment"})
+    keymap.set("n", "<LEADER>-", "<C-x>", { desc = "Decrement"})
+
+    -- Window commands
+    keymap.set("n", "<LEADER>sv", "<C-w>v", { desc = "Vertical split"})
+    keymap.set("n", "<LEADER>sh", "<C-w>h", { desc = "Horizontal split"})
+    keymap.set("n", "<LEADER>se", "<C-w>=", { desc = "Make split equal width"})
+    keymap.set("n", "<LEADER>sx", "<CMD>close<CR>", { desc = "Close current split"})
+
+    -- Tabs
+    keymap.set("n", "<LEADER>to", "<CMD>tabnew<CR>", { desc = "Open new tab"})
+    keymap.set("n", "<LEADER>tx", "<CMD>tabclose<CR>", { desc = "Close tab"})
+    keymap.set("n", "<LEADER>tn", "<CMD>tabn<CR>", { desc = "Go to next tab"})
+    keymap.set("n", "<LEADER>tp", "<CMD>tabp<CR>", { desc = "Go to previous tab"})
 end
 return M
