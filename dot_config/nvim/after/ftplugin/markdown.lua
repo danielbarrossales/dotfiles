ef324ed099af86ba610f8695c5fd302d2453d041
Daniel Barros Sales <daniel.b.sales@outlook.com>
2024-04-24 09:38:48 -0300

    config(nvim): remove telekasten plugin

--- dot_config/nvim/after/ftplugin/markdown.lua
+++ dot_config/nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,10 @@
 vim.opt.colorcolumn = ""
 vim.opt.wrap = true
+vim.cmd[[
+highlight link vimwikiHeader2 @markup.heading.2.markdown
+highlight link vimwikiHeader3 @markup.heading.3.markdown
+highlight link vimwikiHeader4 @markup.heading.4.markdown
+highlight link vimwikiHeader5 @markup.heading.5.markdown
+highlight link vimwikiHeader6 @markup.heading.6.markdown
+]]
+
--- dot_config/nvim/lua/plugins/notes.lua
+++ dot_config/nvim/lua/plugins/notes.lua
@@ -20,31 +20,21 @@ return {
             \ "### What didn't go to plan?",  "* ",
             \ "### What did I learn?",  "* " ])
             ]])
+            vim.cmd[[
+            set nocompatible
+            filetype plugin on
+            syntax on
+            ]]
         end
     },
     {
-        "renerocksai/telekasten.nvim",
-        dependencies = {"nvim-telescope/telescope.nvim"},
-        keys = {
-            -- Launch panel if nothing is typed after <leader>z
-            { "<leader>z", "<cmd>Telekasten panel<CR>" },
-            -- Most used functions
-            { "<leader>zf", "<CMD>Telekasten find_notes<CR>" },
-            { "<leader>zg", "<cmd>Telekasten search_notes<CR>" },
-            { "<leader>zd", "<cmd>Telekasten goto_today<CR>" },
-            { "<leader>zz", "<cmd>Telekasten follow_link<CR>" },
-            { "<leader>zn", "<cmd>Telekasten new_note<CR>" },
-            { "<leader>zc", "<cmd>Telekasten show_calendar<CR>" },
-            { "<leader>zb", "<cmd>Telekasten show_backlinks<CR>" },
-            { "<leader>zI", "<cmd>Telekasten insert_img_link<CR>" },
-            -- Call insert link automatically when we start typing a link
-            { "[[", "<cmd>Telekasten insert_link<CR>", mode = "i" },
-        },
-        opts = {
-            home = vim.fn.expand("~/Documents/vimwiki"),
-            auto_set_filetype = false,
-            auto_set_syntax = false,
-            new_note_filename = "uuid-title"
-        },
+        "WnP/vimwiki_markdown"
+    },
+    "tools-life/taskwiki",
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
     },
 }
